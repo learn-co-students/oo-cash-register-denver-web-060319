a9c05bcff898b38026e074a3d552f04d09082d97
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-14 13:06:06 -0600

    Build CashRegister class
    Practice using instance methods and variables to accomplish various tasks within and pertaining to a class

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require "pry"
 
+class CashRegister
+
+  attr_reader :name, :discount
+  attr_accessor :total, :items, :last_transaction
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    while quantity > 0 do
+      @items << title
+      quantity -= 1
+    end
+    @last_transaction = {title => price}
+  end
+
+  def apply_discount
+    if discount != 0
+      @total = (@total * ((100 - @discount)/100.0)).to_i
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @last_transaction[@items.last]
+  end
+
+end
